1654666b28f955ec39dda7091ba0aced0499b2dc
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-04-10 22:30:19 +0300

    New commit

--- src/main/java/com/updatingworks/springbootupdating/GradeSubmissionApplication.java
+++ src/main/java/com/updatingworks/springbootupdating/GradeSubmissionApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootUpdatingApplication {
+public class GradeSubmissionApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootUpdatingApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(GradeSubmissionApplication.class, args);
+	}
 
 }
