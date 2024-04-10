801256463b63c9cad366487702920fb5f835841e
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-04-10 21:10:50 +0300

    Initial commit

---
+++ src/main/java/com/updatingworks/springbootupdating/SpringBootUpdatingApplication.java
@@ -0,0 +1,13 @@
+package com.updatingworks.springbootupdating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUpdatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUpdatingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/updatingworks/springbootupdating/SpringBootUpdatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.updatingworks.springbootupdating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUpdatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
