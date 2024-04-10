63ecb1e10e403ae79080b0a1c7864608a7fe53f8
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-04-10 22:59:46 +0300

    New commit

--- src/main/java/com/updatingworks/springbootupdating/Grade.java
+++ src/main/java/com/updatingworks/springbootupdating/Grade.java
@@ -1,49 +1,36 @@
-package com.updatingworks.springbootupdating;
-
-import java.util.UUID;
+package com.ltp.gradesubmission;
 
 public class Grade {
     private String name;
     private String subject;
     private String score;
-    private String id;
 
-
-    public Grade() {
-        this.id = UUID.randomUUID().toString();
+    public Grade(String name, String subject, String score){
+        this.name = name;
+        this.subject = subject;
+        this.score = score;
     }
-
-    public String getName() {
-        return name;
-    }
+    public Grade(){}
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getSubject() {
-        return subject;
     }
 
     public void setSubject(String subject) {
         this.subject = subject;
     }
 
-    public String getScore() {
-        return score;
-    }
-
     public void setScore(String score) {
         this.score = score;
     }
 
-
-    public String getId() {
-        return this.id;
+    public String getName(){
+        return this.name;
     }
-
-    public void setId(String id) {
-        this.id = id;
+    public String getSubject(){
+        return this.subject;
     }
-
+    public String getScore(){
+        return score;
+    }
 }
